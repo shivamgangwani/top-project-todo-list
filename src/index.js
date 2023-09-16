1c5efea86f3b559d6eebeae33a8bc2645048c77f
Shivam Gangwani <shivam.gangwani2804@gmail.com>
2023-09-16 14:28:13 +0530

    Add CSS
    Just style normalization for now

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
 import Project from "./modules/project";
+import "./style.css"
 
-let project = new Project("Test", "ABCD");
-
-project.createToDo("Todo1", "Just testing");
-project.createToDo("Todo2", "Just testing");
-project.createToDo("Todo3", "Just testing");
+const CONTENT = document.querySelector("div#content");
 
 
-console.log(project.getPendingToDos());
-console.log(project.getPendingToDosCount());
-
-project.todos[0].setDoneState(true);
-project.todos[1].setPriority("High");
-project.todos[2].setDueAt("16-Sep-2023 21:00:00");
-
-console.log(project.viewToDos());
+(function init() {
+    // Create the skeleton of the page
+    // 1. Sidebar with list of projects
+    // 2. Remaining section to display todos
+    CONTENT.textContent = "ABC";
+})();
